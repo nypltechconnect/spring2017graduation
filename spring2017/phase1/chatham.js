dce00c95551f152c0cd259bcf4ff6580a163f0b9
iansudderth <iansudderth@nypl.org>
2017-08-09 16:02:58 +0000

    cullen and chatham fixes

--- spring2017/phase1/chatham.js
+++ spring2017/phase1/chatham.js
@@ -1,1 +1,52 @@
-projects  = [{"Location": "Chatham Square Tuesday & Thursday 5:30pm -7:30pm", "Instructor": "Aaron McGuire", "Personal Projects": [{"Name": "Laura Pelcher", "url": "https://laurapelcher.github.io/kitchenencyclopedia/meat.html"}, {"Name": "Louise Zhao", "url": "https://wheezymonster.github.io/traveljournal/index.html"}, {"Name": "Paulino Figueirido", "url": "https://pfigs1.github.io/first/"}, {"Name": "Patricia Diaz", "url": "https://dyazbba.github.io/exploredr/index.html"}, {"Name": "Richard Andrew", "url": "https://raileyfinn.github.io/juice/index.html"}, {"Name": "Kestrel Ambrose", "url": "https://klambrose.github.io/projectcode-ambrose"}], "Group Projects": [{"Name": "Winery Group", "URL": "https://klambrose88.github.io/projectcode-group/index.html"}, {"Name": "Bird Group", "URL": "https://pfigs1.github.io/birdsofny/index.html"}, {"Name": "ExploreNYC", "URL": "https://dyazbba.github.io/explorenyc/index.html"}]}]
+projects = [
+	{
+		Location: "Chatham Square Tuesday & Thursday 5:30pm -7:30pm",
+		Instructor: "Aaron McGuire",
+		"Personal Projects": [
+			{
+				Name: "Laura Pelcher",
+				url:
+					"https://laurapelcher.github.io/kitchenencyclopedia/meat.html"
+			},
+			{
+				Name: "Louise Zhao",
+				url: "https://wheezymonster.github.io/traveljournal/index.html"
+			},
+			{
+				Name: "Paulino Figueirido",
+				url: "https://pfigs1.github.io/first/"
+			},
+			{
+				Name: "Patricia Diaz",
+				url: "https://dyazbba.github.io/exploredr/index.html"
+			},
+			{
+				Name: "Richard Andrew",
+				url: "https://raileyfinn.github.io/juice/index.html"
+			},
+			{
+				Name: "Kestrel Ambrose",
+				url: "https://klambrose88.github.io/projectcode-ambrose/"
+			},
+			{
+				Name: "Karla Filippelli",
+				url: "https://nycnyc.github.io/FranceProvence/index.html"
+			}
+		],
+		"Group Projects": [
+			{
+				Name: "Winery Group",
+				URL:
+					"https://klambrose88.github.io/projectcode-group/index.html"
+			},
+			{
+				Name: "Bird Group",
+				URL: "https://pfigs1.github.io/birdsofny/index.html"
+			},
+			{
+				Name: "ExploreNYC",
+				URL: "https://dyazbba.github.io/explorenyc/index.html"
+			}
+		]
+	}
+];
